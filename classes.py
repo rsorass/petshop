9e0152474942ad8011cf58373b2fd2209c226365
Heitor Soares <heitorsramosh@gmail.com>
2026-09-17 15:55:06 -0300

    Classe Gato criada

--- classes.py
+++ classes.py
@@ -15,9 +15,8 @@ class Cliente():
         self.historico = []
         self.pets = []
 
-    def adicionar_ou_adotar_pet(self, pet, idade):
+    def adicionar_ou_adotar_pet(self, pet):
         self.pets.append(pet)
-        Pet(pet, idade)
 
     def adicionar_compra(self, compra):
         self.historico.append(compra)
@@ -27,6 +26,14 @@ class Pet():
         self.nome = nome
         self.idade = idade
 
+class Gato(Pet):
+    def __init__(self, nome, idade, cor, raca):
+        super().__init__(nome, idade)
+
+        self.cor = cor
+        self.raca = raca
+
+
 class Servicos():
     def __init__(self, servico, preco_servico):
         self.servico = servico
@@ -34,6 +41,8 @@ class Servicos():
 
 joao = Cliente("João")
 joao.adicionar_compra("Arranhador")
+pet_joao = Gato("Anubis", 1, "Preto", "SRD")
+joao.adicionar_ou_adotar_pet(pet_joao)
+print(joao.pets[0].nome)
+joao.adicionar_compra("Caixa de areia")
 print(joao.historico)
-joao.adicionar_ou_adotar_pet("Anubis", 1)
-print(joao.pets)
