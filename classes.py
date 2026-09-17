7552169490b85bf7deca3b93f80e487e11fe76ed
Heitor Soares <heitorsramosh@gmail.com>
2026-09-17 17:21:31 -0300

    classes e banco de dados criados

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
 class VendasEHistorico():
-    def __init__(self, produto, preco, data):
-        self.produto = produto
-        self.preco = preco
+    def __init__(self, cliente: "Cliente", pet: "Pet", servico, data):
+        self.cliente = cliente
+        self.pet = pet
+        self.servico = servico
         self.data = data
+        self.valor_total = servico.preco
+
+        self.cliente.adicionar_compra(self)
 
 class Usuario():
     def __init__(self, nome, senha):
@@ -33,6 +37,19 @@ class Gato(Pet):
         self.cor = cor
         self.raca = raca
 
+    
+    def miar(self):
+        print(f"{self.nome}: Meow!")
+
+class Cachorro(Pet):
+    def __init__(self, nome, idade, cor, raca):
+        super().__init__(nome, idade)
+
+        self.cor = cor
+        self.raca = raca
+
+    def latir(self):
+        print(f"{self.nome}: Woof!")
 
 class Servicos():
     def __init__(self, servico, preco_servico):
@@ -40,9 +57,15 @@ class Servicos():
         self.preco = preco_servico
 
 joao = Cliente("João")
-joao.adicionar_compra("Arranhador")
+banho = Servicos("Banho Completo", 50.00)
 pet_joao = Gato("Anubis", 1, "Preto", "SRD")
 joao.adicionar_ou_adotar_pet(pet_joao)
-print(joao.pets[0].nome)
-joao.adicionar_compra("Caixa de areia")
-print(joao.historico)
+print("Pet(s):")
+for pet in joao.pets:
+    print(pet.nome)
+print(f"Serviço: {banho.servico} | Preço: {banho.preco}")
+nova_venda = VendasEHistorico(joao, pet_joao, banho, "17/09/2026")
+print(f"O cliente {nova_venda.cliente.nome} comprou {nova_venda.servico.servico} para seu pet {nova_venda.pet.nome} no dia {nova_venda.data}")
+print(f"Valor a pagar: R${nova_venda.valor_total}")
+pet_joao.miar()
+print(joao.historico[0].servico.servico)
--- datactrl.py
+++ datactrl.py
@@ -2,3 +2,11 @@ import sqlite3
 conexao = sqlite3.connect('database.db')
 cursor = conexao.cursor()
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                usuario TEXT NOT NULL,
+                senha TEXT NOT NULL
+                )""")
+
+conexao.commit()
+conexao.close()
