009f0619e542796f470785d1fb537be4669a787b
Heitor Soares <heitorsramosh@gmail.com>
2026-09-22 16:39:17 -0300

    Algumas coisas da interface alteradas e agora o banco de dados possui novas tabelas para produtos

--- datactrl.py
+++ datactrl.py
@@ -19,6 +19,58 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS pets (
                 FOREIGN KEY (dono_id) REFERENCES usuarios(id)
                 )""")
 
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS comidas (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                comida TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS brinquedos (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                brinquedo TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS casinhas (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                casinha TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS utilitarios (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                utilitario TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS serviços (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                serviço TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL,
+                data TEXT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS historico (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                produto_ou_serviço TEXT NOT NULL,
+                imagem_url TEXT,
+                preço FLOAT NOT NULL,
+                data TEXT NOT NULL,
+
+                cliente_id INTEGER,
+                FOREIGN KEY (cliente_id) REFERENCES usuarios(id)
+)""")
+
+cursor.execute("""INSERT INTO comidas
+                (comida, imagem_url, preço) VALUES
+                ("Ração Premium Para Cachorro Adulto", "https://images.unsplash.com/photo-1684882726821-2999db517441?q=80&w=300", 89.90)""")
+
 user_procurado = None
 cursor.execute("SELECT * FROM usuarios WHERE usuario = ?", (user_procurado,))
 resultado = cursor.fetchone()
